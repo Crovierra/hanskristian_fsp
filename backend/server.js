e3bb593b64680e5baa77e4f685f0a63fa591af66
Hans Kristian <hanskristtian.t@gmail.com>
2025-10-06 12:41:21 +0700

    fix: server.js cors

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT
 app.use(express.json())
 app.use(cors({
   origin: process.env.FRONTEND_URL,
-  methods: ["GET", "POST", "DELETE", "PATCH", "PUT"],
+  methods: ["GET", "POST", "DELETE", "PATCH", "PUT", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }))
